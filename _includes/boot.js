7595af552dfcc21e24ce002284ea088eb88b318e
aimeesohn <aimee@wiredcraft.com>
2012-12-17 11:55:56 +0800

    update

--- _includes/boot.js
+++ _includes/boot.js
@@ -1,2 +1,12 @@
-// Boot
-// ----
+---
+---
+{% include vendor/jquery-1.8.2.min.js %}
+{% include vendor/underscore-min.js %}
+{% include vendor/backbone-min.js %}
+{% include vendor/fancybox.js %}
+
+{% include util.js %}
+{% include model.js %}
+{% include routers/router.js %}
+{% include views/header.js %}
+{% include views/main.js %}
