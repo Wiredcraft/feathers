f6046ed0d5daa3fa24c735a96da0100f9e2a1438
Ronan Berder <ronan@wiredcraft.com>
2013-01-15 17:22:29 +0800

    Fixing typos and layout

--- _includes/view/model.js
+++ _includes/view/model.js
@@ -11,7 +11,7 @@
                     callback(res.toJSON());
                 },
                 error: function(coll, res) {
-                    console.log('errrr', arguments,res.status);
+                    console.log('error', arguments,res.status);
                     switch (res.status) {
                         case 403:
                             app.routers.navigate('/', {trigger: true});
@@ -34,35 +34,6 @@
                 callback($(that.el));
             });
         }
-
-        // renderModel: function (callback) {
-        //     var that = this;
-        //     this.model.fetch({
-        //         silent: true,
-        //         success: function(res) {
-
-        //             var model = res.toJSON();
-        //             $(that.el).html( that.template({ model: model }) );
-
-        //             callback($(that.el), model);
-        //         },
-        //         error: function(coll, res) {
-                    
-        //             switch (res.status) {
-        //                 case 403:
-        //                     app.routers.navigate('/', {trigger: true});
-        //                     break;
-        //                 case 404:
-        //                     // TODO: handle 404 Not Found
-        //                     break;
-        //                 case 500:
-        //                     // TODO: handle 500 Internal Server Error
-        //                     break;
-        //             }
-
-        //         }
-        //     });
-        // }
 
     });
 
