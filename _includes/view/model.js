f1972def768e2ca3e685af2ab2d8a03666eab296
aimeesohn <aimee@wiredcraft.com>
2013-01-15 18:20:26 +0800

    cleaning

--- _includes/view/model.js
+++ _includes/view/model.js
@@ -35,35 +35,6 @@
             });
         }
 
-        // renderModel: function (callback) {
-        //     var that = this;
-        //     this.model.fetch({
-        //         silent: true,
-        //         success: function(res) {
-
-        //             var model = res.toJSON();
-        //             $(that.el).html( that.template({ model: model }) );
-
-        //             callback($(that.el), model);
-        //         },
-        //         error: function(coll, res) {
-                    
-        //             switch (res.status) {
-        //                 case 403:
-        //                     app.routers.navigate('/', {trigger: true});
-        //                     break;
-        //                 case 404:
-        //                     // TODO: handle 404 Not Found
-        //                     break;
-        //                 case 500:
-        //                     // TODO: handle 500 Internal Server Error
-        //                     break;
-        //             }
-
-        //         }
-        //     });
-        // }
-
     });
 
 }).call(this, app.views);
