289ab35ec5d0d7664675c67b415439ab112ee38a
aimeesohn <aimee@wiredcraft.com>
2013-04-17 14:12:09 +0800

    cleaned up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,10 +7,7 @@ config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     when('/', {templateUrl: 'app/views/main.html',   controller: MainCtrl}).
     when('/city/:countryName/:cityName', {templateUrl: 'app/views/detail-city.html', controller: CityCtrl}).
-    when('/cities', {templateUrl: 'app/views/list-city.html',   controller: CitiesCtrl}).
     when('/countries/:countryName', {templateUrl: 'app/views/detail-country.html', controller: CountryCtrl}).
-    when('/countries', {templateUrl: 'app/views/list-country.html',   controller: CountriesCtrl}).
     when('/regions/:regionId', {templateUrl: 'app/views/detail-region.html', controller: RegionCtrl}).
-    when('/regions', {templateUrl: 'app/views/list-region.html',   controller: RegionsCtrl}).
     otherwise({redirectTo: '/'});
 }]);
--- app/scripts/controller/controllers.js
+++ app/scripts/controller/controllers.js
@@ -2,48 +2,23 @@
 
 /* Controllers */
 
-function MainCtrl($scope, $routeParams, City, Region) {
-    // $scope.cities = City.get({cityName:$routeParams.cityName});
-    // $scope.regions = Region.get({regionId:$routeParams.regionId})
-    // $scope.cities = City.query();
-    // $scope.countries = Country.query();
-    // $scope.regions = Region.query();
-    
-    // $scope.client = Clients.get({id:$routeParams.clientId})
-    // $scope.bills = ClientBills.get({clientId:$routeParams.clientId})
+function MainCtrl($scope, $routeParams, City, Country, Region) {
+    $scope.cities = City.query();
+    $scope.countries = Country.query();
+    $scope.regions = Region.query();
 }
 
 /* City */
-function CitiesCtrl($scope, City) {
-    $scope.cities = City.query();
-}
-//CitiesCtrl.$inject = ['$scope', 'City'];
-
 function CityCtrl($scope, $routeParams, City) {
     $scope.city = City.get({cityName: $routeParams.cityName});
 }
-//CityCtrl.$inject = ['$scope', '$routeParams', 'City'];
-
 
 /* Country */
-function CountriesCtrl($scope, Country) {
-    $scope.countries = Country.query();
-}
-//CountriesCtrl.$inject = ['$scope', 'Country'];
-
 function CountryCtrl($scope, $routeParams, Country) {
     $scope.country = Country.get({countryName: $routeParams.countryName});
 }
-//CountryCtrl.$inject = ['$scope', '$routeParams', 'Country'];
-
 
 /* Region */
-function RegionsCtrl($scope, Region) {
-    $scope.regions = Region.query();
-}
-//RegionsCtrl.$inject = ['$scope', 'Region'];
-
 function RegionCtrl($scope, $routeParams, Region) {
     $scope.region = Region.get({regionId: $routeParams.regionId});
 }
-//RegionCtrl.$inject = ['$scope', '$routeParams', 'Region'];
