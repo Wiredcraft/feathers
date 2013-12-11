151a12afb53b0dfda37fff4cc31be31ba967f3b7
fraserxu <xvfeng123@gmail.com>
2013-12-11 17:07:10 +0800

    update all npm modules

--- grunt/tasks/cli_dev.js
+++ grunt/tasks/cli_dev.js
@@ -1,11 +1,9 @@
 module.exports = function(grunt) {
     grunt.registerTask('dev', [
         'ngconstant:development',
+
         'build',
         'copy:build_appjs',
-        'headers:build',
-        'connect',
-        'shell',
-        'watch'
+        'headers:build'
     ]);
 }
