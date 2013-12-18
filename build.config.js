c63425649c8c40ac80f4666799ca2ce475e514a8
fraserxu <xvfeng123@gmail.com>
2013-12-18 17:44:23 +0800

    rewrite grunt file

--- build.config.js
+++ build.config.js
@@ -1,62 +1,37 @@
-/**
- * This file/module contains all configuration for the build process.
- */
 module.exports = {
-  /**
-   * The `build_dir` folder is where our projects are compiled during
-   * development and the `compile_dir` folder is where our app resides once it's
-   * completely built.
-   */
+
   build_dir: 'build',
   compile_dir: 'bin',
 
-  /**
-     * This will change the port to the app server port
-     */
-  "dev_port": 3000,
+  "dev_port": 5000,
 
-  /**
-   * This is a collection of file patterns that refer to our app code (the
-   * stuff in `src/`). These file paths are used in the configuration of
-   * build tasks. `js` is all project javascript, less tests. `ctpl` contains
-   * our reusable components' (`src/common`) template HTML files, while
-   * `atpl` contains the same, but for our app's code. `html` is just our
-   * main HTML file, `less` is our main stylesheet, and `unit` contains our
-   * app's unit tests.
-   */
   app_files: {
-    js: [ 'src/**/*.js', '!src/**/*.spec.js' ],
-    jsunit: [ 'src/**/*.spec.js' ],
+    js: ['src/**/*.js', '!src/**/*.spec.js'],
+    jsunit: ['src/**/*.spec.js'],
 
-    atpl: [ 'src/app/**/*.tpl.html' ],
-    ctpl: [ 'src/common/**/*.tpl.html' ],
+    atpl: ['src/app/**/*.tpl.html'],
+    ctpl: ['src/common/**/*.tpl.html'],
 
-    html: [ 'src/index.html' ],
-    scss: [ "src/scss/**/*.{scss,sass}" ]
+    html: ['src/index.html'],
+    scss: ['src/scss/**/*.{scss,sass}']
   },
 
-  /**
-   * This is the same as `app_files`, except it contains patterns that
-   * reference vendor code (`vendor/`) that we need to place into the build
-   * process somewhere. While the `app_files` property ensures all
-   * standardized files are collected for compilation, it is the user's job
-   * to ensure non-standardized (i.e. vendor-related) files are handled
-   * appropriately in `vendor_files.js`.
-   *
-   * The `vendor_files.js` property holds files to be automatically
-   * concatenated and minified with our project source files.
-   *
-   * The `vendor_files.css` property holds any CSS files to be automatically
-   * included in our app.
-   */
+  test_files: {
+    js: [
+        'vendor/angular-mocks/angular-mocks.js'
+    ]
+  },
+
   vendor_files: {
     js: [
-      'vendor/angular/angular.js',
-      'vendor/angular-ui-router/release/angular-ui-router.js'
+      "vendor/angular/angular.js",
+      'vendor/angular-ui-router/release/angular-ui-router.min.js',
+      'vendor/angular-ui-utils/modules/route/route.js'
     ],
-    css: [
-    ],
-    img: [
+    css: [],
+    assets: [
+        'vendor/normalize-css/normalize.css',
+        'vendor/offline/themes/offline-theme-dark.css'
     ]
   },
 
